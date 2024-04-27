b0056e4d3c41ecbcce520c665f91cf1ac0cfa6bb
dyzal127 <rbca22127.rcca@gmail.com>
2024-04-27 13:02:41 +0530

    commit

---
+++ casetools_se_set7/src/casetools_se_set7exam/Rectangle.java
@@ -0,0 +1,34 @@
+package casetools_se_set7exam;
+
+public class Rectangle {
+	private double width;
+	private double height ;
+	public Rectangle (double width , double height ) {
+		this.width=width;
+		this.width=width;
+		
+	}
+	public double getWidth() {
+		return width;
+		
+	}
+	public void setWidth(double width) {
+		this.width=width;
+		
+	}
+	public double getHeight() {
+		return height;
+		
+	}
+	public void setHeight(double Height) {
+		this.height=Height;
+		
+	}
+	public void main(String[]args) {
+		//TODO Auto-generated method stub
+	}
+
+}
+
+
+
---
+++ casetools_se_set7/src/casetools_se_set7exam/book.java
@@ -0,0 +1,31 @@
+package casetools_se_set7exam;
+
+public class book  {
+	private double width;
+	private double height ;
+	public book (double width , double height ) {
+		this.width=width;
+		this.width=width;
+		
+	}
+	public double getWidth() {
+		return width;
+		
+	}
+	public void setWidth(double width) {
+		this.width=width;
+		
+	}
+	public double getHeight() {
+		return height;
+		
+	}
+	public void setHeight(double Height) {
+		this.height=Height;
+		
+	}
+	public void main(String[]args) {
+		//TODO Auto-generated method stub
+	}
+
+}
